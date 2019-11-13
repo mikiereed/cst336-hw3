da5655defea0a13f2f63084158c6f0db3126e89e
EC2 Default User <ec2-user@ip-172-31-48-179.us-west-2.compute.internal>
2019-11-13 05:25:33 +0000

    try again

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
         <title> Weather App </title>
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script type="text/javascript" src="js/main.js"></script>
     </head>
     <body>
         <h1 class="jumbotron"> Weather Update </h1>
